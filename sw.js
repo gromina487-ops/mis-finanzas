1e0f37d8c496db31499a86bf97bae891f8cf7680
gromina487-ops <gromina487@gmail.com>
2026-08-01 19:44:36 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "finanzas-cache-v1";
+const CACHE_NAME = "finanzas-cache-v2";
 const FILES_TO_CACHE = ["./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
